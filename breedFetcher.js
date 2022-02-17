de51197518974ea35ddd32b80bc2e39b705b33eb
Alex <alex.gillespie@live.com>
2022-02-17 18:50:55 -0500

    Created index.js file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,18 @@
 const request = require("request");
-const breed = process.argv.splice(2);
 
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
-request(url, (err, response, body) => {
-  if (err) throw err;
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (err, response, body) => {
+    if (err) throw err;
 
-  const data = JSON.parse(body);
-  if (!data.length) {
-    return console.log('No breed found');
-  }
-  const breed = data[0].description;
-  if (breed) {
-    console.log(breed);
-  }
-  
-});
+    const data = JSON.parse(body);
+   
+    const breed = data[0];
+    if (breed) {
+      return callback(null, breed.description);
+    }
+    callback('Failed to find breed');
+  });
+};
 
+module.exports = { fetchBreedDescription };
